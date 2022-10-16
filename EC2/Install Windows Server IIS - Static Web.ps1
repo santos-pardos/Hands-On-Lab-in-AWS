4257e94f7d9794f59edffded59e884727a1d75c1
santos-pardos <66181446+santos-pardos@users.noreply.github.com>
2022-10-16 15:13:02 +0200

    Add files via upload

---
+++ EC2/Install Windows Server IIS - Static Web.ps1
@@ -0,0 +1,18 @@
+﻿<powershell> 
+
+
+# Install IIS
+Import-Module ServerManager; 
+Install-WindowsFeature web-server, web-webserver -IncludeAllSubFeature; 
+Install-WindowsFeature web-mgmt-tools; 
+
+
+# Install Static Website
+Start-Sleep -Seconds 120;
+Remove-item -recurse c:\inetpub\wwwroot\*; 
+(New-Object System.Net.WebClient).DownloadFile("https://sanvalero-static-webs.s3.eu-west-1.amazonaws.com/Drink-Water.zip", "c:\inetpub\wwwroot\Drink-Water.zip"); 
+Start-Sleep -Seconds 10;
+Expand-Archive -Path "c:\inetpub\wwwroot\Drink-Water.zip" -DestinationPath "c:\inetpub\wwwroot" -Force;
+
+
+</powershell>
