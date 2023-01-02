a33736596d83b566e8173ca64086ba7ba389025d
santos-pardos <66181446+santos-pardos@users.noreply.github.com>
2023-01-02 12:45:09 +0100

    Delete Dockers directory

--- Dockers/Docker_Basics_101/README.md
+++
@@ -1,5 +0,0 @@
-# Starting with Dockers
-
-https://youtu.be/eAI6qcC8_t4
-
-https://docs.docker.com/get-started/02_our_app/
--- Dockers/Docker_NodeJs/check_server.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Welcome to Node.js Server');
-}).listen(3000, "0.0.0.0");
-console.log('Server running at http://myippublica:3000/');
