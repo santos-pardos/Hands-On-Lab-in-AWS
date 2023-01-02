c7ae0a7e373825afc2b836b5211e4cdfa6519cfd
santos-pardos <66181446+santos-pardos@users.noreply.github.com>
2023-01-02 12:44:40 +0100

    Add files via upload

---
+++ Compute/EC2/ELB-ASG/Stress.sh
@@ -0,0 +1,2 @@
+amazon-linux-extras install epel -y
+yum install stress -y
---
+++ Compute/EC2/Install Linux EC2 - Static Web.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+yum update -y
+yum install httpd -y
+systemctl start httpd
+systemctl enable httpd
+cd /var/www/html
+wget https://sanvalero-static-webs.s3.eu-west-1.amazonaws.com/breakout.zip
+unzip breakout.zip
