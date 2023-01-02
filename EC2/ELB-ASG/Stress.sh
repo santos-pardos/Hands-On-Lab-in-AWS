1fdc0c4b5915f44fe412fc325119fea777a979d1
santos-pardos <66181446+santos-pardos@users.noreply.github.com>
2023-01-02 12:44:53 +0100

    Delete EC2 directory

--- EC2/ELB-ASG/Stress.sh
+++
@@ -1,2 +0,0 @@
-amazon-linux-extras install epel -y
-yum install stress -y
--- EC2/Install Linux EC2 - Static Web.sh
+++
@@ -1,8 +0,0 @@
-﻿#!/bin/bash
-yum update -y
-yum install httpd -y
-systemctl start httpd
-systemctl enable httpd
-cd /var/www/html
-wget https://sanvalero-static-webs.s3.eu-west-1.amazonaws.com/breakout.zip
-unzip breakout.zip
