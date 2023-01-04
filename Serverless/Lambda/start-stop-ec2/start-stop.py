c69a326a713d0637d6f922163cc81cf545146636
santos-pardos <66181446+santos-pardos@users.noreply.github.com>
2023-01-04 19:13:23 +0100

    Update start-stop.py

--- Serverless/Lambda/start-stop-ec2/start-stop.py
+++ Serverless/Lambda/start-stop-ec2/start-stop.py
@@ -1,9 +1,8 @@
 import boto3
-region = 'eu-west-1'
-  instances = ['i-0d6a9b386e8e2553a']
-# instances = ['i-049eb3775de4a3d2d']
+region = 'us-east-1'
+instances = ['i-0fc2a84093c2cd1de']
 ec2 = boto3.client('ec2', region_name=region)
 def lambda_handler(event, context):
-    ec2.stop_instances(InstanceIds=instances)
-#    ec2.start_instances(InstanceIds=instances)
+   # ec2.stop_instances(InstanceIds=instances)
+    ec2.start_instances(InstanceIds=instances)
     print('stopped your instances: ' + str(instances))
