d137c124c1cf968b8349f301256ad942442d184d
santos-pardos <66181446+santos-pardos@users.noreply.github.com>
2023-01-20 14:33:31 +0100

    Add files via upload

---
+++ Compute/Lambda/Start-Stop-EC2/Stop.py
@@ -0,0 +1,8 @@
+import boto3
+region = 'us-west-1'
+instances = ['i-12345cb6de4f78g9h', 'i-08ce9b2d7eccf6d26']
+ec2 = boto3.client('ec2', region_name=region)
+
+def lambda_handler(event, context):
+    ec2.stop_instances(InstanceIds=instances)
+    print('stopped your instances: ' + str(instances))
