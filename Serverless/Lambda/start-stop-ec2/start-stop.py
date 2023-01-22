daacdeac8d04126ab6608c3ddc9613d503394399
santos-pardos <66181446+santos-pardos@users.noreply.github.com>
2023-01-22 23:23:59 +0100

    Delete Serverless/Lambda directory

--- Serverless/Lambda/start-stop-ec2/start-stop.py
+++
@@ -1,8 +0,0 @@
-import boto3
-region = 'us-east-1'
-instances = ['i-0fc2a84093c2cd1de']
-ec2 = boto3.client('ec2', region_name=region)
-def lambda_handler(event, context):
-   # ec2.stop_instances(InstanceIds=instances)
-    ec2.start_instances(InstanceIds=instances)
-    print('stopped your instances: ' + str(instances))
