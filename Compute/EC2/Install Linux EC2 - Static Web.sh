eb5f1bdd8615fedfbb12bd9b49010b514d198de3
santos-pardos <66181446+santos-pardos@users.noreply.github.com>
2023-09-16 19:28:28 +0200

    Update Install Linux EC2 - Static Web.sh

--- Compute/EC2/Install Linux EC2 - Static Web.sh
+++ Compute/EC2/Install Linux EC2 - Static Web.sh
@@ -1,8 +1,23 @@
-﻿#!/bin/bash
+Linux AMI 2
+
+ #!/bin/bash
 yum update -y
 yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
+cd /var/www/html
+wget https://sanvalero-static-webs.s3.eu-west-1.amazonaws.com/breakout.zip
+unzip breakout.zip
+
+
+Linux AMI 2023
+
+#!/bin/bash
+dnf update -y
+dnf install nginx -y
+systemctl start nginx
+systemctl enable nginx
+systemctl status nginx
 cd /var/www/html
 wget https://sanvalero-static-webs.s3.eu-west-1.amazonaws.com/breakout.zip
 unzip breakout.zip
