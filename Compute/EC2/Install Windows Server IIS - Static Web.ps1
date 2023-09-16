2367761475d688f03d095abc1d4554b26d9b0748
santos-pardos <66181446+santos-pardos@users.noreply.github.com>
2023-09-16 19:52:01 +0200

    Update Install Windows Server IIS - Static Web.ps1

--- Compute/EC2/Install Windows Server IIS - Static Web.ps1
+++ Compute/EC2/Install Windows Server IIS - Static Web.ps1
@@ -6,7 +6,7 @@ Install-WindowsFeature web-server, web-webserver -IncludeAllSubFeature;
 Install-WindowsFeature web-mgmt-tools; 
 
 # Install Static Website
-Start-Sleep -Seconds 120;
+Start-Sleep -Seconds 300;
 Remove-item -recurse c:\inetpub\wwwroot\*; 
 (New-Object System.Net.WebClient).DownloadFile("https://sanvalero-static-webs.s3.eu-west-1.amazonaws.com/Drink-Water.zip", "c:\inetpub\wwwroot\Drink-Water.zip"); 
 Start-Sleep -Seconds 10;
