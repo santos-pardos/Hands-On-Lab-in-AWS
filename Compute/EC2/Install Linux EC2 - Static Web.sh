7002a9f05c8864ff53cd022fc746d183a550777b
santos-pardos <66181446+santos-pardos@users.noreply.github.com>
2023-09-26 00:03:34 +0200

    Update Install Linux EC2 - Static Web.sh

--- Compute/EC2/Install Linux EC2 - Static Web.sh
+++ Compute/EC2/Install Linux EC2 - Static Web.sh
@@ -22,3 +22,14 @@ cd /usr/share/nginx/html
 rm index.html
 wget https://sanvalero-static-webs.s3.eu-west-1.amazonaws.com/breakout.zip
 unzip breakout.zip
+
+
+#!/bin/bash
+dnf update -y
+dnf install nginx -y
+systemctl start nginx
+systemctl enable nginx
+cd /usr/share/nginx/html
+rm index.html
+wget https://s3.eu-west-1.amazonaws.com/www.profesantos.cloud/Netflix.zip
+unzip Netflix.zip
