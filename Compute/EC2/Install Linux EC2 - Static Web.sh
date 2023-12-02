91d62bfe6847d458e44b64ae8458180622a3f456
santos-pardos <66181446+santos-pardos@users.noreply.github.com>
2023-12-02 09:44:30 +0100

    Update Install Linux EC2 - Static Web.sh

--- Compute/EC2/Install Linux EC2 - Static Web.sh
+++ Compute/EC2/Install Linux EC2 - Static Web.sh
@@ -9,6 +9,14 @@ cd /var/www/html
 wget https://sanvalero-static-webs.s3.eu-west-1.amazonaws.com/breakout.zip
 unzip breakout.zip
 
+#!/bin/bash
+# install httpd (Linux 2 version)
+yum update -y
+yum install -y httpd.x86_64
+systemctl start httpd.service
+systemctl enable httpd.service
+echo "Microservicio 1 desde $(hostname -f)" > /var/www/html/index.html
+
 
 Linux AMI 2023
 
@@ -33,3 +41,14 @@ cd /usr/share/nginx/html
 rm index.html
 wget https://s3.eu-west-1.amazonaws.com/www.profesantos.cloud/Netflix.zip
 unzip Netflix.zip
+
+#!/bin/bash 
+sudo dnf update -y 
+sudo dnf install -y docker 
+service docker start 
+systemctl enable docker.service
+docker pull santospardos/upc:juiceshop
+docker run -d -p 80:3000 santospardos/upc:juiceshop
+
+
+
