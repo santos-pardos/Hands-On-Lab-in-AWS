4519a746c59ddb0484bb25ca484601fc8daf7c3c
santos-pardos <66181446+santos-pardos@users.noreply.github.com>
2024-02-14 09:46:12 +0100

    Update Install Linux EC2 - Static Web.sh

--- Compute/EC2/Install Linux EC2 - Static Web.sh
+++ Compute/EC2/Install Linux EC2 - Static Web.sh
@@ -58,10 +58,7 @@ apt-get update && apt-get upgrade -y
 apt-get install apache2 git -y
 systemctl enable apache2
 systemctl start apache2
-cd ..
-cd ..
-cd var
-cd www
-git clone https://github.com/samcolon/LUITProject19.git
-cp -R /var/www/LUITProject19/HolidayGiftsWebsite/* /var/www/html/
+cd /var/wwww/html
+wget https://s3.eu-west-1.amazonaws.com/www.profesantos.cloud/HolidayGiftsWebsite.zip
+unzip HolidayGiftsWebsite.zip
 
