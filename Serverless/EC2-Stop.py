003bb3ead4e6269a1f7884686352605e5449d05e
santos-pardos <66181446+santos-pardos@users.noreply.github.com>
2024-02-19 23:42:42 +0100

    Update EC2-Stop.py

--- Serverless/EC2-Stop.py
+++ Serverless/EC2-Stop.py
@@ -1,1 +1,8 @@
+import boto3
+region = 'us-east-1'
+instances = ['i-0c52a844896d4ef7f', 'i-0ead9736115d43e28']
+ec2 = boto3.client('ec2', region_name=region)
 
+def lambda_handler(event, context):
+    ec2.stop_instances(InstanceIds=instances)
+    print('stopped your instances: ' + str(instances))
