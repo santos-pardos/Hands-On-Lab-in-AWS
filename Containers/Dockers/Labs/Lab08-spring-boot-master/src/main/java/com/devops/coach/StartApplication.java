109ee61835cee399f2cbbf51e732b2398c63ddfd
santos-pardos <66181446+santos-pardos@users.noreply.github.com>
2024-03-12 13:37:00 +0100

    Add files via upload

---
+++ Containers/Dockers/Labs/Lab08-spring-boot-master/src/main/java/com/devops/coach/StartApplication.java
@@ -0,0 +1,24 @@
+package com.devops.coach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class StartApplication {
+
+    @GetMapping("/")
+    public String index(final Model model) {
+        model.addAttribute("title", "Howdy Folks..Welcome to Devops Coaching by Coach AK!");
+        model.addAttribute("msg", "Hello All..We are deploying springboot application into EKS cluster using Helm + Jenkins Pipeline!!!! Today is Aug 7th, 2023");
+        return "index";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+
+}
