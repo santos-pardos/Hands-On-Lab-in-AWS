aba92b5e158b915d8f0ef5dcc58329ffc0373bde
profesantos <66181446+santos-pardos@users.noreply.github.com>
2024-04-18 13:41:02 +0200

    Update Install Linux EC2 - Static Web.sh

--- Compute/EC2/Install Linux EC2 - Static Web.sh
+++ Compute/EC2/Install Linux EC2 - Static Web.sh
@@ -9,6 +9,17 @@ systemctl start httpd
 systemctl enable httpd
 echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
 
+#!/bin/bash
+sudo su
+yum update -y
+yum install -y httpd
+cd /var/www/html
+wget [https://github.com/azeezsalu/jupiter/archive/refs/heads/main.zip](https://s3.eu-west-1.amazonaws.com/www.profesantos.cloud/jupiter-website.zip)
+unzip jupiter-website.zip)
+cp -r jupiter-main/* /var/www/html/
+rm -rf jupiter-main main.zip
+systemctl enable httpd 
+systemctl start httpd
 
 #!/bin/bash
 yum update -y
