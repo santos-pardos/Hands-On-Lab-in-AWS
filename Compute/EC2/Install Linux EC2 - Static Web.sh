dab8b0cbeff2c3accc179219138e69e2b8205a07
profesantos <66181446+santos-pardos@users.noreply.github.com>
2026-05-05 00:53:18 +0200

    Update Install Linux EC2 - Static Web.sh

--- Compute/EC2/Install Linux EC2 - Static Web.sh
+++ Compute/EC2/Install Linux EC2 - Static Web.sh
@@ -67,6 +67,16 @@ rm index.html
 https://s3.eu-west-1.amazonaws.com/www.profesantos.cloud/Web-CV.zip
 unzip Web-CV.zip
 
+#!/bin/bash
+#Install Apache Web Server and PHP
+yum install -y httpd mysql php
+#Download Lab files
+wget https://aws-tc-largeobjects.s3.us-west-2.amazonaws.com/CUR-TF-100-RESTRT-1/267-lab-NF-build-vpc-web-server/s3/lab-app.zip
+unzip lab-app.zip -d /var/www/html/
+#Turn on web server
+chkconfig httpd on
+service httpd start
+
 
 #!/bin/bash
 dnf update -y
